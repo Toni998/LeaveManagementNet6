661f8c30a7cd17b632a456ce091490928017dc56
Antonio Golemac <agolemac3@gmail.com>
2022-09-12 12:01:47 +0200

    Add project files.

---
+++ LeaveManagement.Data/Migrations/20220827093250_HoursAddedToRequest.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeaveManagement.Data.Migrations
+{
+    public partial class HoursAddedToRequest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "LeaveTypes",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestingEmployeeId",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SelectedHours",
+                table: "LeaveRequests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeId",
+                table: "LeaveAllocations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a6e-f7bb-4448-baaf-1add431ccbbf",
+                column: "ConcurrencyStamp",
+                value: "2941c6fd-6df9-46e2-8330-19b096f7c125");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a7e-f7cb-4148-baaf-1acb431eabbf",
+                column: "ConcurrencyStamp",
+                value: "0446d029-5e22-4b6a-b81d-7be3984c28dd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3f4631bd-f907-4409-b416-ba356312e659",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2bde7d1e-fe4c-4724-bb7c-4b6fa9191337", "AQAAAAEAACcQAAAAEPzK4C6i6/DB7DHImXmF26iqKci53nvZ6GYS+YLo8o6pUBAjsh3rjbL7y5VJ9txVMA==", "1a08e59a-5a27-4844-9f70-2403b072070b" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "408aa945-3d84-4421-8342-7269ec64d949",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5f445639-fc33-41f3-b04a-bcda4d159669", "AQAAAAEAACcQAAAAEKgY+f3F0iOkVB0+ywTT3reRoZlDfuu633em4bqyzSM4E/cqq4286pwGKZ7lUhGxgw==", "fa931b68-2fc5-469b-9ca7-0e611f0d1b46" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SelectedHours",
+                table: "LeaveRequests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "LeaveTypes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RequestingEmployeeId",
+                table: "LeaveRequests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmployeeId",
+                table: "LeaveAllocations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a6e-f7bb-4448-baaf-1add431ccbbf",
+                column: "ConcurrencyStamp",
+                value: "4c0527fc-a9e6-4451-b8c7-3a10b7aca0fa");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "cac43a7e-f7cb-4148-baaf-1acb431eabbf",
+                column: "ConcurrencyStamp",
+                value: "ea9f3515-f510-48aa-9c3f-8b699ba94405");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "3f4631bd-f907-4409-b416-ba356312e659",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "c9c3828e-9b86-4e80-8e4e-adce0b9066f5", "AQAAAAEAACcQAAAAEGJqNyCGluiBWtgQyE4Fqi5R6FIkMoeE5hb3Ae2xBrOdWMId7qysngXV3nKnBdS9TQ==", "528f44bd-6885-4d4d-9539-5a7c4849c273" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "408aa945-3d84-4421-8342-7269ec64d949",
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "295770ce-3f4b-4677-b0af-8d200fb60b8f", "AQAAAAEAACcQAAAAEEUIUqWq294RI6lX0MJC8VPeZH2T/jwLXg2Urd+V5xoJssKchHIlExmnxZy5UB/rpw==", "a43230f0-c9b2-406b-ab50-cd0905055fd1" });
+        }
+    }
+}
